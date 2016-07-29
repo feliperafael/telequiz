8edf6642bd5e4d0bb1ec74b6a4f3a6c32184a015
eduardorochasoares <rsoares.eduardo@gmail.com>
2016-07-29 19:55:28 -0300

    Arquivos do NCLUA
    Adicionada a parte NCL

---
+++ NCL/perguntas.lua
@@ -0,0 +1,20 @@
+perguntas = {
+	{
+	  per = "Quem deve ser julgado?",
+	  resp = {"Cersei e Loras", "Tommem e Margaery", "Jamie e Cersei", "Qyburn e Cersei"},
+	  corr = 3
+	},
+
+    {
+      per = "Qual o nome do atual técnico da seleção brasileira?",
+      resp = {"Filipão", "Mano Menezes", "Dunga"},
+      corr = 2
+    },
+    
+    {
+      per = "Quem ficou de fora da seleção brasileira em 2010?",
+      resp = { "Ronaldo", "Neimar", "Robinho", "Ronaldinho Gaúcho"},
+      corr = 1
+    },    
+	
+}
